fc43fc9c343ba45d50ffe7ae25fe979e007f9659
asirera <antonio_sirera@yahoo.es>
2014-02-26 06:38:04 +0000

    initialcommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Midemoapp::Application.config.secret_token = 'f711b3440d12488336bb08a804da4d5c602c259cfe0a38268de57fe5839e4a80daeeb30514d44b406458455868e98beeb13528508511c202f947584472520b20'
